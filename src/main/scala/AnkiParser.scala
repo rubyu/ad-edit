b1160e65ab6d3a5d04b0f46de95abf885fc8a126
rubyu <ruby.u.g@gmail.com>
2014-05-28 12:42:23 +0900

    Reforctoring AnkiParser and AnkiParserTest.

--- src/main/scala/AnkiParser.scala
+++ src/main/scala/AnkiParser.scala
@@ -4,31 +4,41 @@ package com.github.rubyu.adupdate
 import util.parsing.combinator.RegexParsers
 
 
+object AnkiParser {
+  object result {
+    trait Element
+    case class Tags(value: String) extends Element
+    case class Comment(value: String) extends Element
+    case class Row(value: List[String]) extends Element
+    case class EOL(value: String) extends Element
+  }
+}
+
 class AnkiParser extends RegexParsers {
   override def skipWhitespace = false
 
-  var firstLine = true
+  def first_line      = tags_res | comment_res | row_res | eol_res
+  def line            =            comment_res | row_res | eol_res
 
-  def line = firstLine match {
-    case true       => tags | comment | row | EOL
-    case false      => comment | row | EOL
-  }
+  def tags_res        = tags    ^^ { AnkiParser.result.Tags(_) }
+  def comment_res     = comment ^^ { AnkiParser.result.Comment(_) }
+  def row_res         = row     ^^ { AnkiParser.result.Row(_) }
+  def eol_res         = EOL     ^^ { AnkiParser.result.EOL(_) }
+
   def tags            = "tags:" ~ rep( not( EOL ) ~> any ) ^^ { case a ~ b => a + b.mkString }
-  def comment         = "#" ~ rep( not( EOL ) ~> any ) ^^ { case a ~ b => a + b.mkString }
+  def comment         = "#"     ~ rep( not( EOL ) ~> any ) ^^ { case a ~ b => a + b.mkString }
   def row             = repsep( field, DELIMITER )
-  def field           = space ~> quoted_field <~ space | raw_value
-  def quoted_field    = rep1( QUOTE ~> quoted_value <~ QUOTE ) ^^ { _.mkString }
-  def space           = rep( SPACER ) ^^^ ""
-  def SPACER          = ' '
-  def QUOTE           = '"'
+
+  def field           = padding ~> quoted_field <~ padding | raw_value
+  def quoted_field    = QUOTE   ~> quoted_value <~ QUOTE
+  def padding         = rep( SPACE )
   def escaped_quote   = QUOTE ~ QUOTE ^^^ QUOTE
   def quoted_value    = rep1( escaped_quote | not( QUOTE ) ~> any | EOL ) ^^ { _.mkString }
   def raw_value       = rep( not( DELIMITER ) ~> any ) ^^ { _.mkString }
-  def any             = """.""".r
+
+  def any             = ".".r
+  def SPACE           = ' '
+  def QUOTE           = '"'
   def DELIMITER       = '\t'
-  def EOL             = rep1( '\r'.? ~ '\n' ) ^^ { _ map {
-      case Some(_) ~ _ => "\r\n"
-      case _ => "\n"
-    } mkString
-  }
+  def EOL             = "\r\n" | "\n"
 }
--- src/test/scala/AnkiParserTest.scala
+++ src/test/scala/AnkiParserTest.scala
@@ -12,7 +12,7 @@ class AnkiParserTest extends SpecificationWithJUnit {
   }
 
   "AnkiParser.raw_value" should {
-    "parse empty input" in new scope {
+    "return empty string value when input is empty" in new scope {
       val result = parser.parse(parser.raw_value, "")
       result.get mustEqual ""
     }
@@ -27,197 +27,288 @@ class AnkiParserTest extends SpecificationWithJUnit {
       result.get mustEqual "abc"
     }
 
-    "parse \\n" in new scope {
+    "return empty string value when input starts with \\n" in new scope {
       val result = parser.parse(parser.raw_value, "\n")
       result.get mustEqual ""
     }
 
-    "parse \\n" in new scope {
+    "return empty string value when input starts with \\r\\n" in new scope {
+      val result = parser.parse(parser.raw_value, "\r\n")
+      result.get mustEqual ""
+    }
+
+    "parse until \\n" in new scope {
       val result = parser.parse(parser.raw_value, "a\nb")
+      result.get mustEqual "a"
+    }
+
+    "parse until \\r\\n" in new scope {
+      val result = parser.parse(parser.raw_value, "a\r\nb")
       result.get mustEqual "a"
     }
   }
 
   "AnkiParser.quoted_value" should {
-    "1" in new scope {
+    "parse a character" in new scope {
       val result = parser.parse(parser.quoted_value, "a")
       result.get mustEqual "a"
     }
 
-    "2" in new scope {
+    "parse characters" in new scope {
+      val result = parser.parse(parser.quoted_value, "abc")
+      result.get mustEqual "abc"
+    }
+
+    "parse a double quote to a single quote" in new scope {
       val result = parser.parse(parser.quoted_value, "\"\"")
       result.get mustEqual "\""
     }
 
-    "parse characters" in new scope {
+    "parse characters contains a double quote" in new scope {
       val result = parser.parse(parser.quoted_value, "a\"\"b")
       result.get mustEqual "a\"b"
     }
 
-    "error when " in new scope {
+    "return empty result when input starts with \"" in new scope {
       val result = parser.parse(parser.quoted_value, "\"")
       result.isEmpty must beTrue
+    }
+
+
+    "allow \\n" in new scope {
+      val result = parser.parse(parser.quoted_value, "\n")
+      result.get mustEqual "\n"
+    }
+
+    "allow \\r\\n" in new scope {
+      val result = parser.parse(parser.quoted_value, "\r\n")
+      result.get mustEqual "\r\n"
+    }
+
+    "parse input contains \\n" in new scope {
+      val result = parser.parse(parser.quoted_value, "a\nb")
+      result.get mustEqual "a\nb"
+    }
+
+    "parse input contains \\r\\n" in new scope {
+      val result = parser.parse(parser.quoted_value, "a\r\nb")
+      result.get mustEqual "a\r\nb"
     }
   }
 
 
   "AnkiParser.quoted_field" should {
-    "1" in new scope {
+    "return empty result when input does not starts with quote character" in new scope {
+      val result = parser.parse(parser.quoted_field, "a")
+      result.isEmpty must beTrue
+    }
+
+    "return empty result when input starts with quote character and breaks in the middle" in new scope {
+      val result = parser.parse(parser.quoted_field, "\"a")
+      result.isEmpty must beTrue
+    }
+
+    "parse input starts and ends with quote character" in new scope {
       val result = parser.parse(parser.quoted_field, "\"a\"")
+      result.get mustEqual "a"
+    }
+
+    "parse input starts and ends with quote character until second quote character" in new scope {
+      val result = parser.parse(parser.quoted_field, "\"a\"b")
       result.get mustEqual "a"
     }
   }
 
   "AnkiParser.field" should {
-    "1" in new scope {
+    "parse quoted text" in new scope {
       val result = parser.parse(parser.field, "\"a\"")
       result.get mustEqual "a"
     }
 
-    "2" in new scope {
+    "parse quoted text with padding before it" in new scope {
       val result = parser.parse(parser.field, " \"a\"")
       result.get mustEqual "a"
     }
 
-    "3" in new scope {
+    "parse quoted text with padding after it" in new scope {
       val result = parser.parse(parser.field, "\"a\" ")
       result.get mustEqual "a"
     }
 
-    "4" in new scope {
+    "parse quoted text with paddings both before and after" in new scope {
       val result = parser.parse(parser.field, " \"a\" ")
       result.get mustEqual "a"
     }
 
-    "6" in new scope {
+    "parse raw-text" in new scope {
+      val result = parser.parse(parser.field, "a")
+      result.get mustEqual "a"
+    }
+
+    "parse raw-text with padding before it as raw-text" in new scope {
       val result = parser.parse(parser.field, " a")
       result.get mustEqual " a"
     }
 
-    "7" in new scope {
+    "parse raw-text with padding after it as raw-text" in new scope {
       val result = parser.parse(parser.field, "a ")
       result.get mustEqual "a "
     }
 
-    "8" in new scope {
+    "parse raw-text with paddings both before and after it as raw-text" in new scope {
       val result = parser.parse(parser.field, " a ")
       result.get mustEqual " a "
     }
 
-    "9" in new scope {
+    "parse single quote as raw-text" in new scope {
       val result = parser.parse(parser.field, "\"")
       result.get mustEqual "\""
     }
 
-    "10" in new scope {
+    "parse text ends with single quote as raw-text" in new scope {
+      val result = parser.parse(parser.field, "a\"")
+      result.get mustEqual "a\""
+    }
+
+    "parse text starts with single quote as raw-text" in new scope {
+      val result = parser.parse(parser.field, "\"a")
+      result.get mustEqual "\"a"
+    }
+
+    "parse text contains single quote as raw-text" in new scope {
+      val result = parser.parse(parser.field, "a\"b")
+      result.get mustEqual "a\"b"
+    }
+
+    "return empty string when input starts with \\n" in new scope {
+      val result = parser.parse(parser.field, "\na")
+      result.get mustEqual ""
+    }
+
+    "return empty string when input starts with \\r\\n" in new scope {
+      val result = parser.parse(parser.field, "\r\na")
+      result.get mustEqual ""
+    }
+
+    "parse raw-text until \\n" in new scope {
+      val result = parser.parse(parser.field, "a\nb")
+      result.get mustEqual "a"
+    }
+
+    "parse raw-text until \\r\\n" in new scope {
+      val result = parser.parse(parser.field, "a\r\nb")
+      result.get mustEqual "a"
+    }
+
+    "parse quoted-text contains \n" in new scope {
       val result = parser.parse(parser.field, "\"a\nb\"")
       result.get mustEqual "a\nb"
     }
 
-    "11" in new scope {
-      val result = parser.parse(parser.field, "a\nb")
-      result.get mustEqual "a"
+    "parse quoted-text contains \r\n" in new scope {
+      val result = parser.parse(parser.field, "\"a\r\nb\"")
+      result.get mustEqual "a\r\nb"
     }
   }
 
   "AnkiParser.row" should {
-    "1" in new scope {
+    "return a list contains a empty string when input is empty" in new scope {
+      val result = parser.parse(parser.row, "")
+      result.get mustEqual List("")
+    }
+
+    "return a list size is 1 when a raw-text is given" in new scope {
       val result = parser.parse(parser.row, "a")
       result.get mustEqual List("a")
     }
 
-    "2" in new scope {
+    "return a list size is 2 when tab separeted 2 raw-text are given" in new scope {
       val result = parser.parse(parser.row, "a\tb")
       result.get mustEqual List("a", "b")
     }
 
-    "3" in new scope {
+    "return a list size is 3 when tab separeted 3 raw-text are given" in new scope {
       val result = parser.parse(parser.row, "a\tb\tc")
       result.get mustEqual List("a", "b", "c")
     }
 
-    "1" in new scope {
+    "return a list size is 1 when a quoted-text is given" in new scope {
       val result = parser.parse(parser.row, "\"a\"")
       result.get mustEqual List("a")
     }
 
-    "2" in new scope {
+    "return a list size is 2 when tab separeted 2 quoted-text is given" in new scope {
       val result = parser.parse(parser.row, "\"a\"\t\"b\"")
       result.get mustEqual List("a", "b")
     }
 
-    "3" in new scope {
+    "return a list size is 3 when tab separeted 3 quoted-text is given" in new scope {
       val result = parser.parse(parser.row, "\"a\"\t\"b\"\t\"c\"")
       result.get mustEqual List("a", "b", "c")
     }
-
   }
 
-  "AnkiParser.coment" should {
-    "1" in new scope {
-      val result = parser.parse(parser.comment, "")
+  "AnkiParser.comment" should {
+    "return empty result when input is empty" in new scope {
+      val result = parser.parse(parser.comment_res, "")
       result.isEmpty must beTrue
     }
 
-    "2" in new scope {
-      val result = parser.parse(parser.comment, "a")
+    "return empty result when input is raw-text" in new scope {
+      val result = parser.parse(parser.comment_res, "a")
       result.isEmpty must beTrue
     }
 
-    "3" in new scope {
-      val result = parser.parse(parser.comment, "#")
-      result.get mustEqual "#"
+    "parse a comment" in new scope {
+      val result = parser.parse(parser.comment_res, "#")
+      result.get mustEqual AnkiParser.result.Comment("#")
     }
 
-    "1" in new scope {
-      val result = parser.parse(parser.comment, "# ")
-      result.get mustEqual "# "
+    "parse a comment until the end" in new scope {
+      val result = parser.parse(parser.comment_res, "#a")
+      result.get mustEqual AnkiParser.result.Comment("#a")
     }
 
-    "2" in new scope {
-      val result = parser.parse(parser.comment, "#\n")
-      result.get mustEqual "#"
+    "parse a comment until until \n" in new scope {
+      val result = parser.parse(parser.comment_res, "#\n")
+      result.get mustEqual AnkiParser.result.Comment("#")
     }
 
-    "3" in new scope {
-      val result = parser.parse(parser.comment, "# \n")
-      result.get mustEqual "# "
+    "parse a comment until until \r\n" in new scope {
+      val result = parser.parse(parser.comment_res, "#\r\n")
+      result.get mustEqual AnkiParser.result.Comment("#")
     }
-
   }
 
   "AnkiParser.tags" should {
-    "1" in new scope {
+    "return empty result when input is empty" in new scope {
       val result = parser.parse(parser.tags, "")
       result.isEmpty must beTrue
     }
 
-    "2" in new scope {
+    "return empty result when input is raw-text" in new scope {
       val result = parser.parse(parser.tags, "a")
       result.isEmpty must beTrue
     }
 
-    "3" in new scope {
+    "parse a tags" in new scope {
       val result = parser.parse(parser.tags, "tags:")
       result.get mustEqual "tags:"
     }
 
-    "1" in new scope {
+    "parse a tags until the end" in new scope {
       val result = parser.parse(parser.tags, "tags: ")
       result.get mustEqual "tags: "
     }
 
-    "2" in new scope {
+    "parse a tags until \n" in new scope {
       val result = parser.parse(parser.tags, "tags:\n")
       result.get mustEqual "tags:"
     }
 
-    "3" in new scope {
-      val result = parser.parse(parser.tags, "tags: \n")
-      result.get mustEqual "tags: "
+    "parse a tags until \r\n" in new scope {
+      val result = parser.parse(parser.tags, "tags:\r\n")
+      result.get mustEqual "tags:"
     }
-
   }
-
-
 }
